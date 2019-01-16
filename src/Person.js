c6bb2558ddf283bbf34af8f3fd022e4c7ebe647e
Zaman Rezaei <zamanphp@gmail.com>
2019-01-16 15:43:24 +0330

    use ajax in person component

--- src/Person.js
+++ src/Person.js
@@ -1,13 +1,52 @@
 import React, { PureComponent } from "react";
+import getPosts from "./api";
 
 class Person extends PureComponent {
+  state = {
+    posts: []
+  };
+
+  componentDidMount() {
+    getPosts().then(response => {
+      this.setState({
+        posts: response.data
+      });
+    });
+  }
+
+  __renderTable = posts => {
+    const html = posts.map(post => {
+      return (
+        <tr key={post.id}>
+          <td>{post.title}</td>
+          <td>{post.body}</td>
+        </tr>
+      );
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>body</th>
+          </tr>
+          {html}
+        </thead>
+      </table>
+    );
+  };
+
   render() {
     console.log("render person");
     const { name, age } = this.props;
+    const { posts } = this.state;
     return (
       <div>
         <h1>name : {name}</h1>
         <h2>age : {age}</h2>
+        <hr />
+        {posts.length > 0 && this.__renderTable(posts)}
       </div>
     );
   }
